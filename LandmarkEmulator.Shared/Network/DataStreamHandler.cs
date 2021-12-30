689e80a4f63753af2462a0807ef124d188cb9837
Kirmmin <kirmmin@hotmail.com>
2021-12-30 14:59:06 -0500

    Data Stream abstraction

--- LandmarkEmulator.Shared/Network/DataStreamHandler.cs
+++ LandmarkEmulator.Shared/Network/DataStreamHandler.cs
@@ -3,6 +3,7 @@ using LandmarkEmulator.Shared.Network.Message.Model;
 using NLog;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace LandmarkEmulator.Shared.Network
@@ -42,12 +43,17 @@ namespace LandmarkEmulator.Shared.Network
 
         public void ProcessDataFragment(DataFragment dataFragment)
         {
-            if (NextSequence == null)
-                NextSequence = dataFragment.Sequence;
+            ReadyDataForProcessing(dataFragment.Sequence, dataFragment.Data, true);
+        }
 
-            if (dataFragment.Sequence > NextSequence)
+        private void ReadyDataForProcessing(ushort sequence, byte[] data, bool isFragment)
+        {
+            if (NextSequence == null)
+                NextSequence = sequence;
+
+            if (sequence > NextSequence)
             {
-                log.Warn($"Sequence out of order, expected {NextSequence} but received {dataFragment.Sequence}");
+                log.Warn($"Sequence out of order, expected {NextSequence} but received {sequence}");
                 _session.OnDisconnect();
 
                 // TODO: Handle out of order packets
@@ -57,7 +63,7 @@ namespace LandmarkEmulator.Shared.Network
             }
 
             int lastAck = LastAck ?? -1;
-            ushort ack = dataFragment.Sequence;
+            ushort ack = sequence;
             for (ushort i = 1; i <= ushort.MaxValue; i++)
             {
                 ushort j = (ushort)((lastAck + i) & 0xFFFF);
@@ -76,7 +82,7 @@ namespace LandmarkEmulator.Shared.Network
                 });
             }
 
-            DataPackets[dataFragment.Sequence] = new DataPacket(dataFragment.Data, true);
+            DataPackets[sequence] = new DataPacket(data, isFragment);
 
             NextSequence = (ushort)((LastAck + 1) & 0xFFFF);
 
@@ -86,6 +92,7 @@ namespace LandmarkEmulator.Shared.Network
         private void ParseDataFragments()
         {
             var nextFragment = (_lastProcessedFragment + 1) & 0xFFFF;
+            List<byte[]> finishedData = new();
 
             if (DataPackets[nextFragment] == null)
                 return;
@@ -94,15 +101,16 @@ namespace LandmarkEmulator.Shared.Network
 
             if (!dataPacket.IsFragment)
             {
-                // Do Stuff! We're ready!
+                _lastProcessedFragment = nextFragment;
+                finishedData = ParseChannelData(dataPacket.Data.ToList());
+                DecryptData(finishedData);
                 return;
             }
-
-            var reader = new GamePacketReader(dataPacket.Data);
-            int totalSize = (int)reader.ReadUInt();
-            int dataSize = dataPacket.Data.Length - 4;
+            
             bool dataReady = false;
-            List<byte[]> finishedData = new();
+            var reader = new GamePacketReader(dataPacket.Data);
+            int totalSize = (int)reader.ReadUIntBE();
+            int dataSize = dataPacket.Data.Length - 4;
 
             List<byte> data = new();
             for (int i = 4; i < dataPacket.Data.Length; i++)
@@ -130,6 +138,7 @@ namespace LandmarkEmulator.Shared.Network
                         _lastProcessedFragment = j;
                         dataReady = true;
                         finishedData = ParseChannelData(data);
+                        log.Info($"Packet with length {totalSize} is ready for parsing!");
                         break;
                     }
                 }
@@ -138,10 +147,7 @@ namespace LandmarkEmulator.Shared.Network
             }
 
             if (dataReady)
-            {
-                log.Info($"Packet with length {totalSize} is ready for parsing!");
                 DecryptData(finishedData);
-            }
         }
 
         private List<byte[]> ParseChannelData(List<byte> data)
@@ -162,11 +168,11 @@ namespace LandmarkEmulator.Shared.Network
                     // sometimes there's an extra 0x00 byte in the beginning that trips up the RC4
                     var reader = new GamePacketReader(piece);
                     byte[] parsedData;
-                    if (piece.Length > 1 && reader.ReadUShort() == 0)
+                    if (piece.Length > 1 && reader.ReadUShortBE() == 0)
                         parsedData = Encryption.RC4.Decrypt(_rc4_key, new Span<byte>(piece, 1, piece.Length - 1).ToArray());
                     else
                         parsedData = Encryption.RC4.Decrypt(_rc4_key, piece);
-                    OnData(parsedData);
+                    OnData(parsedData); // Emit the parsed data packet to subscribers
                 }
             }
         }
