84d638e573adfda31577182002ea2d8181d82ccb
Kirmmin <kirmmin@hotmail.com>
2023-10-01 14:01:59 -0400

    [Zone/Packets] WE'RE HOME... Sort of. Enter world is finally working. You can remain in world and run around without crashes (though uber laggy). Packet dumps are being used, currently, to "fake" entering.

---
+++ LandmarkEmulator.WorldServer/Network/Message/Model/ClientFinishedLoading.cs
@@ -0,0 +1,13 @@
+﻿using LandmarkEmulator.Shared.Network;
+using LandmarkEmulator.Shared.Network.Message;
+
+namespace LandmarkEmulator.WorldServer.Network.Message.Model
+{
+    [ZoneMessage(ZoneMessageOpcode.ClientFinishedLoading)]
+    public class ClientFinishedLoading : IReadable
+    {
+        public void Read(GamePacketReader reader)
+        {
+        }
+    }
+}
